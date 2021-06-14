5ea368dbbb785c4d94917a9310025d6573e37527
sunxinyu <sunxinyu@yucekj.com>
2021-06-14 11:35:43 +0800

    docs: add doc

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import cssColors from "./css-colors";
+import ColorsFactory from "./factory";
 
 export const logger = (...args) => {
   const styles = [];
@@ -17,73 +17,6 @@ export const logger = (...args) => {
 };
 
 export const colors = () => {
-  const inst = function (o) {
-    if (o) this.o = o;
-    return this;
-  };
-  Object.assign(inst, {
-    styles: [],
-    o: "",
-    log(o) {
-      this.o = o;
-      return this;
-    },
-    hexColor(hexColor) {
-      this.styles.push(`color:${hexColor};`);
-      return this;
-    },
-    color(color) {
-      this.styles.push(`color:${color};`);
-      return this;
-    },
-    bg(color) {
-      this.styles.push(`background:${color};`);
-      return this;
-    },
-    padding(size, sizeH) {
-      if (sizeH) {
-        this.styles.push(`padding:${size}px ${sizeH}px;`);
-      } else {
-        this.styles.push(`padding:${size}px;`);
-      }
-      return this;
-    },
-    bold(o) {
-      this.styles.push(`font-weight:bold;`);
-      if (o) this.o = o;
-      return this;
-    },
-    underline(o) {
-      this.styles.push(`font-style:underline;`);
-      if (o) this.o = o;
-      return this;
-    },
-    italic(o) {
-      this.styles.push(`font-style:italic;`);
-      if (o) this.o = o;
-      return this;
-    },
-    result() {
-      return {
-        o: this.o,
-        style: this.styles.join(""),
-      };
-    },
-  });
-
-  cssColors.forEach((color) => {
-    Object.defineProperty(inst, color, {
-      get() {
-        this.styles.push(`color:${color};`);
-        return this;
-      },
-    });
-    Object.defineProperty(inst, color + "Bg", {
-      get() {
-        this.styles.push(`background:${color};`);
-        return this;
-      },
-    });
-  });
-  return inst;
+  const instance = ColorsFactory.create();
+  return instance;
 };
